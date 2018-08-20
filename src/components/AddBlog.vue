9d069e69c25c9f0195f5727bdf353e88a2a82ef0
Sun Hao <sjxajhhaozi@foxmail.com>
2018-08-20 09:45:35 +0800

    frist

---
+++ src/components/AddBlog.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="add-blog">
+
+    <h2>添加博客</h2>  
+      <form>
+        <label>博客标题</label>
+        <input type="text" v-model="title" required >
+        <label>博客内容</label>
+        <textarea v-model="content"></textarea>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "add-blog",
+  data() {
+    return {
+      blog:{
+        title:"1232222222111111112222222222",
+        content:""
+      }
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
