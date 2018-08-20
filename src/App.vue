08e7f035f9ccb4c8a86cb17e0dd768124cb36001
Sun Hao <sjxajhhaozi@foxmail.com>
2018-08-20 18:58:49 +0800

    work-win

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
   <div id="app">
     <add-blog> </add-blog>
+    <show-blog></show-blog>
   </div>
 </template>
 
 <script>
 import AddBlog from './components/AddBlog'
+import ShowBlog from './components/ShowBlog'
 
 export default {
   name: 'App',
   components: {
-    AddBlog
+    AddBlog,
+    ShowBlog
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
